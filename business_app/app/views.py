a90ca63a4be420fd2c6b5db0223721d9f71002b8
Elsie Adu <aduelsie@gmail.com>
2017-11-20 17:10:38 -0500

    Error checking and added Username to navigate to profile
    Checks if user has a valid email format and displays errors. Makes sure that the user cannot navigate to any other pages unless they have logged in or signed up. User can click on their username to go to profile. Will work on profile later.

--- business_app/app/views.py
+++ business_app/app/views.py
@@ -15,73 +15,106 @@ def addUser():
 #Route for the index or home page
 @b_app.route('/', methods=['GET', 'POST'])
 def home():
-	pw = ""
-	acct = ""
-	err = ""
-	if request.method == 'POST':
-		#if the sign in button was pressed
-		data = request.form['signSubmit']
-		if request.form['signSubmit'] == 'submitted':
-			s = shelve.open('acct.db')	#open the database
-			#check if the account has been created, where user_key is the email
-			try:
-				print("trying...")
-				if str(request.form.get('user_key')) in s:
-					#check to see if password matches the user's actual password
-					if s[str(request.form.get('user_key'))]['password'] == str(request.form.get('user_password')):
-						loggedIn = True
-						currUser = str(request.form.get('user_key'))
-						return redirect(url_for('index'))
+    pw = ""
+    acct = ""
+    err = ""
+
+    #if the user is logged in, do not allow them to go back to register
+    print currUser
+    if currUser != "":
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        #if the sign in button was pressed
+        data = request.form['signSubmit']
+        if request.form['signSubmit'] == 'submitted':
+            s = shelve.open('acct.db')	#open the database
+            #check if the account has been created, where user_key is the email
+            try:
+                print("trying...")
+                if str(request.form.get('user_key')) in s:
+                    #check to see if password matches the user's actual password
+                    if s[str(request.form.get('user_key'))]['password'] == str(request.form.get('user_password')):
+                        loggedIn = True
+                        currUser = s[str(request.form.get('user_key'))]['name']
+                        return redirect(url_for('index'))
 					#if not failure, display output
-					else:
-						pw = "Password is incorrect. Please try again."
-						return render_template('register.html', pw=pw, acct=acct)
-				else:
+                    else:
+                        pw = "Invalid credentials. Incorrect password or email."
+                        return render_template('register.html', pw=pw, acct=acct)
+                else:
 					acct = "User account is not registered."
 					print(acct)
 
-			except KeyError:
+            except KeyError:
 				print "Account not registered"
-		elif request.form['signSubmit'] == 'signUpSubmit':
-			print("in signup")
-			#get the name, acct, password for the user when they sign up
-			name = str(request.form.get('username'))
-			acct = str(request.form.get('useremail'))
-			pw = str(request.form.get('userpass'))
-			#store it in the database if successful
-			message = userAcct(name, acct, pw)
-			#if the database returns a failure
-			if message == 'failure':
-				err = "Invalid credentials. Email is already taken. Please try again"
-				return render_template("register.html", err=err)
-		#if no error take them to index page
-			else:
+        elif request.form['signSubmit'] == 'signUpSubmit':
+            print("in signup")
+            #get the name, acct, password for the user when they sign up
+            name = str(request.form.get('username'))
+            acct = str(request.form.get('useremail'))
+            pw = str(request.form.get('userpass'))
+            #store it in the database if successful
+            message = userAcct(name, acct, pw)
+            #if the database returns a failure
+            if message == 'failure':
+                err = "Email is already taken. Please try again"
+                return render_template("register.html", err=err)
+            elif re.search("[@.]", acct) is None:
+                err = "Please enter a valid email address"
+                print "Invalid Email Format"
+                return render_template("register.html", err=err)
+            #if no error take them to index page
+            else:
 				err = ""
 				loggedIn = True
 				global currUser
-				currUser = acct
+				currUser = name
 				return redirect(url_for('index'))
 
 
-	#create the view for index.html
-	return render_template('register.html', err=err)
+    #create the view for register.html
+    return render_template('register.html', err=err)
 
 @b_app.route('/index.html', methods=['GET', 'POST'])
 def index():
-	#create the view for the index
-	return render_template('index.html')
+    if currUser == "":
+        return redirect(url_for('home'))
+
+    #create the view for the index
+    return render_template('index.html')
+
 @b_app.route('/clothing.html')
 def signup():
-	return render_template('clothing.html')
+    if currUser == "":
+        return redirect(url_for('home'))
+
+    return render_template('clothing.html')
+
 @b_app.route('/agenda.html')
 def newthing():
-        return render_template('agenda.html')
+    if currUser == "":
+        return redirect(url_for('home'))
+
+    return render_template('agenda.html')
+
 @b_app.route('/interviewQA.html')
 def newinterview():
-	return render_template('interviewQA.html')
+    if currUser == "":
+        return redirect(url_for('home'))
+
+    return render_template('interviewQA.html')
+
 @b_app.route('/Calendar.html')
 def newdate():
-	return render_template('Calendar.html')
+    if currUser == "":
+        return redirect(url_for('home'))
+
+    return render_template('Calendar.html')
+
 @b_app.route('/about.html')
 def aboutus():
-	return render_template('about.html')
+    if currUser == "":
+        return redirect(url_for('home'))
+
+    return render_template('about.html')
